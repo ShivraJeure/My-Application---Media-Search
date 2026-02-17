3cb637351e0fcf5791bf1a5307018228803afc5a
ShivraJeure <shivrajjeure12345@gmail.com>
2026-02-17 15:58:07 +0530

    Fix import path casing for searchSlice

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { setQuery } from '../redux/features/searchSlice'
+import { setQuery } from '../redux/Features/searchSlice'
 
 const SearchBar = () => {
 
